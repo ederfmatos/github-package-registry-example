c5227d31e08f4a1fb11286656bcfdd358a61c381
Eder Matos <eder.matos@exactaworks.com.br>
2021-01-29 15:27:29 -0300

    first commit

---
+++ src/main/java/com/exactaworks/githubpackageregistryexample/ListUtils.java
@@ -0,0 +1,15 @@
+package com.exactaworks.githubpackageregistryexample;
+
+import java.util.List;
+
+public class ListUtils {
+
+    public static boolean isEmpty(List<?> list) {
+        return list == null || list.isEmpty();
+    }
+
+    public static boolean isNotEmpty(List<?> list) {
+        return !isEmpty(list);
+    }
+
+}
